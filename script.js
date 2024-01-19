a3eb731994d56359487d8018eba721f3c6702981
Izaan Khan <137004982+izaankhan0@users.noreply.github.com>
2024-01-19 21:08:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -19,6 +19,5 @@ function clicker() {
     setTimeout(() => {
         box.classList.remove("true");
         box.classList.remove("false");
-        box.innerHTML = "<h1>Press</h1>";
     }, 5000);
 }
